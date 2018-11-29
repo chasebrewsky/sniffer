60af755424998ffa4a40f3ab9f8f6caa226bafc0
chasebrewsky <chasepbrewer@gmail.com>
2018-11-29 00:37:50 -0500

    expanded the client and server code

--- src/GroupClient.java
+++ src/GroupClient.java
@@ -1,3 +1,253 @@
-public class GroupClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Scanner;
 
+class Request {
+  public int timeout;
+  public int size;
+  public URL target;
+  public Request(String input) throws Exception {
+    String[] arguments =  input.split(" ", 3);
+    if (arguments.length < 3) {
+      throw new Exception("input must contain three separate arguments");
+    }
+    size = Integer.parseInt(arguments[0]);
+    timeout = Integer.parseInt(arguments[1]);
+    target = new URL(arguments[2]);
+  }
+
+  public String toString() {
+    return size + " " + timeout + " " + target.toString();
+  }
+}
+
+class Result {
+  private String HTTP = null;
+  private String UDP = null;
+  private boolean failed = false;
+
+  public void setHTTP(String HTTP) { this.HTTP = HTTP; }
+
+  public void setUDP(DatagramPacket[] packets) {
+    StringBuilder builder = new StringBuilder();
+    for (DatagramPacket packet : packets) {
+      builder.append(packet.toString());
+    }
+    UDP = builder.toString();
+  }
+
+  public void timeout() { failed = true; }
+
+  public boolean timedout() { return failed; }
+
+  public boolean equals() {
+    if (HTTP == null || UDP == null) { return false; }
+    return HTTP.equals(UDP);
+  }
+}
+
+class UDPThread extends Thread {
+  private Result result;
+  public UDPThread(DatagramSocket socket, Request request, Result result) throws IOException {
+
+  }
+}
+
+class HTTPThread extends Thread {
+  private URL target;
+  private Result result;
+
+  public HTTPThread(Request request, Result result) {
+    this.target = request.target;
+    this.result = result;
+  }
+
+  public void run() {
+    try { process(); } catch (Exception error) { error.printStackTrace(); }
+  }
+
+  public void process() throws Exception {
+    HttpURLConnection conn = (HttpURLConnection) target.openConnection();
+    conn.setRequestMethod("GET");
+
+    // Turn the input stream into a buffered reader to stream all data into a singular string.
+    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    StringBuilder result = new StringBuilder();
+    String line;
+    while ((line = reader.readLine()) != null) {
+      result.append(line);
+    }
+    this.result.setUDP(this.createPackets(result.toString()));
+  }
+
+  private DatagramPacket[] createPackets(String payload) {
+    String[] segments = this.splitPayload(payload);
+    DatagramPacket[] packets = new DatagramPacket[segments.length];
+    for (int i = 0; i < segments.length; i++) {
+      byte[] converted = segments[i].getBytes();
+      packets[i] = new DatagramPacket(converted, converted.length, address, port);
+    }
+    return packets;
+  }
+
+  /**
+   * Segments the given string into an array of strings that max out at the given limit.
+   * @param payload String to segment into individual strings.
+   * @return Segmented strings from the original string.
+   */
+  private String[] splitPayload(String payload) {
+    int length = payload.length() / size;
+    String[] messages = new String[length];
+    int offset = size;
+    int cursor;
+
+    for (int i = 0; i < length; i++) {
+      cursor = i * size;
+      offset = offset + size;
+      messages[i] = payload.substring(cursor, offset);
+    }
+
+    return messages;
+  }
+}
+
+class GroupClientThread extends Thread {
+  private DatagramSocket socket;
+  private Result result = new Result();
+
+  public void run() {
+    try { process(); } catch (Exception error) { error.printStackTrace(); }
+  }
+
+  private void process() throws Exception {
+    socket = new DatagramSocket();
+
+    System.out.println("UDP Client");
+    System.out.println("----------");
+    System.out.println("Please enter the payload size, request timeout, and web server URL separated by spaces:");
+
+    Request request = read();
+    spawn(request);
+
+    if (result.timedout() && !retryUDP(request)) {
+      System.out.println("quit");
+      return;
+    }
+
+    if (!result.equals()) {
+      System.out.println("Results match!");
+    } else {
+      System.out.println("Results do not match!");
+    }
+  }
+
+  private void spawn(Request request) throws IOException, InterruptedException {
+    Thread[] threads = new Thread[2];
+    threads[0] = processUDP(request);
+    threads[1] = processHTTP(request);
+    for (Thread thread : threads) {
+      thread.start();
+    }
+    for (Thread thread : threads) {
+      thread.join();
+    }
+  }
+
+  private boolean retryUDP(Request request) throws IOException, InterruptedException {
+    send("fail");
+    request.timeout = request.timeout * 2;
+    Thread thread = processUDP(request);
+    thread.start();
+    thread.join();
+    if (result.timedout()) {
+      send("fail");
+      return false;
+    }
+    return true;
+  }
+
+  private UDPThread processUDP(Request request) throws IOException {
+    return new UDPThread(socket, request, result);
+  }
+
+  private HTTPThread processHTTP(Request request) {
+    return new HTTPThread(request, result);
+  }
+
+  private void send(String data) throws IOException {
+    byte[] message = data.getBytes();
+    DatagramPacket packet = new DatagramPacket(message, message.length, InetAddress.getLocalHost(), 13231);
+    socket.send(packet);
+  }
+
+  private void fail() throws IOException {
+    send("fail");
+  }
+
+  private static Request read() {
+    Request request = null;
+    boolean reading = true;
+    while (reading) {
+      System.out.print("(payload, timeout, URL) > ");
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      try {
+        request = new Request(input);
+      } catch (Exception error) {
+        System.out.println("Incorrect format: " + error.getMessage());
+        System.out.println("Please try again:");
+        continue;
+      }
+      reading = false;
+    }
+    return request;
+  }
+}
+
+public class GroupClient {
+  private DatagramSocket socket;
+  public static void main(String args[]) throws IOException {
+    new GroupClientThread().run();
+    socket = new DatagramSocket();
+
+    System.out.println("UDP Client");
+    System.out.println("----------");
+    System.out.println("Please enter the payload size, request timeout, and web server URL separated by spaces:");
+
+
+    byte[] buffer = request.toString().getBytes();
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 13231);
+    socket.send(packet);
+
+    Result result = new Result();
+    UDPThread udp = new UDPThread(socket, packet, request.timeout, result);
+    HTTPThread http = new HTTPThread(request.target, result);
+
+    udp.start();
+    http.start();
+    udp.join();
+    http.join();
+
+    if (result.timedout()) {
+
+      udp = new UDPThread(socket, packet, request.timeout * 2, result);
+      udp.start();
+      udp.join();
+
+      if (result.timedout()) {
+        buffer = new String("")
+        packet = new Data
+        socket
+      }
+    }
+
+    if (result.equals()) {
+      System.out.println("Result match!");
+      return;
+    }
+
+
+  }
 }
--- src/GroupServer.java
+++ src/GroupServer.java
@@ -2,128 +2,250 @@ import java.net.*;
 import java.io.*;
 import java.util.logging.*;
 
-public class GroupServer extends Thread {
-  // Port that the server will listen on.
-  private static final int PORT = 13231;
+/**
+ * Represents a request from the client application.
+ */
+class TransmitRequest {
+  // Target payload size of each returned request.
+  public int size;
+  // Port number to send results back to.
+  public int port;
+  // Timeout for the request.
+  public int timeout;
+  // Address to send request results back to.
+  public InetAddress address;
+  // Target URL to retrieve data from.
+  public URL target;
+
+  /**
+   * Segments the given string into an array of strings that max out at the given limit.
+   * @param payload String to segment into individual strings.
+   * @return Segmented strings from the original string.
+   */
+  private String[] splitPayload(String payload) {
+    int length = payload.length() / size;
+    String[] messages = new String[length];
+    int offset = size;
+    int cursor;
+
+    for (int i = 0; i < length; i++) {
+      cursor = i * size;
+      offset = offset + size;
+      messages[i] = payload.substring(cursor, offset);
+    }
+
+    return messages;
+  }
+
+  /**
+   * Transforms a string of data into packets that adhere to the maximum payload size of
+   * the request.
+   * @param payload String data received from the performed GET request.
+   * @return Segmented datagram packets adhering to the maximum payload size.
+   */
+  private DatagramPacket[] createPackets(String payload) {
+    String[] segments = this.splitPayload(payload);
+    DatagramPacket[] packets = new DatagramPacket[segments.length];
+    for (int i = 0; i < segments.length; i++) {
+      byte[] converted = segments[i].getBytes();
+      packets[i] = new DatagramPacket(converted, converted.length, address, port);
+    }
+    return packets;
+  }
+
+  /**
+   * Creates a request from a given UDP packet.
+   * @param packet UDP packet to pull request parameters from.
+   * @throws IOException If the given data packet does not match the expected arguments.
+   */
+  public TransmitRequest(Packet packet) throws Exception {
+    // Ensure that the given packet is a Transmit packet.
+    if (packet.action != Action.Transmit) {
+      throw new IllegalArgumentException("transmit requests can only be made from transmit action packets");
+    }
+
+    // Parse out attributes from packet.
+    address = packet.address;
+    port = packet.port;
+    parse(packet.data);
+  }
+
+  /**
+   * Parses packet string data into object's arguments.
+   * @param data Packet string data to parse.
+   * @throws Exception If the arguments cannot be parsed.
+   */
+  private void parse(String data) throws Exception {
+    // Split initial input from input stream into file arguments.
+    String[] arguments = data.split(" ", 3);
+
+    // Validate length of input stream arguments.
+    if (arguments.length != 3) {
+      throw new Exception("input stream does not contain 3 arguments");
+    }
+
+    // Attempt to parse integer values from first two input arguments.
+    try {
+      size = Integer.parseInt(arguments[0]);
+      timeout = Integer.parseInt(arguments[1]);
+    } catch (NumberFormatException err) {
+      throw new Exception("payload size or timeout are not integer values");
+    }
+
+    // Extract the target url from the last argument.
+    target = new URL(arguments[2]);
+  }
+
+  /**
+   * Performs the request and returns the appropriate data packets.
+   * @return Segmented data packets.
+   * @throws IOException
+   */
+  public DatagramPacket[] perform() throws IOException {
+    HttpURLConnection conn = (HttpURLConnection) target.openConnection();
+    conn.setRequestMethod("GET");
+
+    // Turn the input stream into a buffered reader to stream all data into a singular string.
+    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    StringBuilder result = new StringBuilder();
+    String line;
+    while ((line = reader.readLine()) != null) {
+      result.append(line);
+    }
+    return this.createPackets(result.toString());
+  }
+}
+
+/**
+ * Enum representation of what a given UDP request means.
+ */
+enum Action {
+  Failure,
+  Success,
+  Transmit
+}
+
+/**
+ * Specific packet class created from the generalized datagram packet.
+ */
+class Packet {
+  private static Action parse(String data) {
+    switch (data) {
+      case "fail":
+        return Action.Failure;
+      case "file OK":
+        return Action.Success;
+      default:
+        return Action.Transmit;
+    }
+  }
+
+  public int port;
+  public InetAddress address;
+  public String data;
+  public Action action;
+
+  /**
+   * Creates a server specific packet from a generalized UDP packet.
+   * @param packet UDP packet to parse data from.
+   */
+  public Packet(DatagramPacket packet) {
+    address = packet.getAddress();
+    port = packet.getPort();
+    data = new String(packet.getData());
+    action = Packet.parse(data);
+  }
+}
+
+class GroupServerThread extends Thread {
   // Private logger for this class.
   private static Logger LOGGER = Logger.getLogger(GroupServer.class.getName());
   // Server instance that will listen for any requests.
   private DatagramSocket socket;
   // Initial buffer to read into.
   private byte[] buffer = new byte[512];
-  private String[] messages;
-
+  // Last known payload information.
+  private DatagramPacket[] payload;
+  private boolean active = true;
 
   /**
    * Creates a new server on the PORT constant.
-   * @throws IOException
+   * @throws IOException If the socket cannot be created.
    */
-  private GroupServer() throws IOException {
-    // Starts server and waits for a connection.
-    socket = new DatagramSocket(GroupServer.PORT);
+  public GroupServerThread(String name) throws IOException {
+    super(name);
+    socket = new DatagramSocket(13231);
+  }
+
+  /**
+   * Performs a transmit request and and sends the contents back to the client.
+   * @param packet Packet to create the request from.
+   * @throws Exception If an error occurs during transmission.
+   */
+  private void transmit(Packet packet) throws Exception {
+    TransmitRequest request = new TransmitRequest(packet);
+    payload = request.perform();
+    send(payload);
   }
 
   /**
    * Implementation of the Thread run function.
+   * Waits to receive a packet from the socket, then
    */
   public void run() {
-    while (true) {
+    System.out.println("Server running on localhost:13231");
+    while (active) {
       try {
         // Accept any new requests to the server.
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
+        DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
+        socket.receive(datagram);
 
-        // Retrieve the address and port to send data back to and recreate the packet to send.
-        InetAddress address = packet.getAddress();
-        int port = packet.getPort();
-
-        String input = new String(packet.getData());
-
-        if (input.equals("fail")) {
-
+        // Transform generalized datagram packet into server specific packet.
+        Packet packet = new Packet(datagram);
+        switch (packet.action) {
+          case Failure:
+            resend();
+          case Success:
+            payload = null;
+          case Transmit:
+            transmit(packet);
         }
+      } catch (IOException error) {
 
-        // Split initial input from input stream into file arguments.
-        String[] arguments = new String(packet.getData()).split(" ", 3);
-
-        // Validate length of input stream arguments.
-        if (arguments.length != 3) {
-          LOGGER.warning("input stream does not contain 3 arguments");
-          continue;
-        }
-
-        // Initialize input stream arguments.
-        int payload;
-        int timeout;
-
-        // Attempt to parse integer values from first two input arguments.
-        try {
-          payload = Integer.parseInt(arguments[0]);
-          timeout = Integer.parseInt(arguments[1]);
-        } catch (NumberFormatException err) {
-          LOGGER.warning("cannot parse integer arguments from input stream");
-          continue;
-        }
-
-        // Pull out get request name.
-        String name = arguments[2];
-
-        // Retrieve the result from the GET request.
-        String result;
-        try {
-          result = this.retrieve(name);
-        } catch (IOException err) {
-          LOGGER.warning(err.getMessage());
-          continue;
-        }
-
-        messages = this.segment(result, payload);
-        for (String message: messages) {
-          byte[] converted = message.getBytes();
-          packet = new DatagramPacket(converted, converted.length, address, port);
-          socket.send(packet);
-        }
-      }
-      catch(IOException err) {
-        LOGGER.severe(err.getMessage());
       }
     }
   }
 
-  private String[] segment(String original, int limit) {
-    int length = original.length() / limit;
-    String[] messages = new String[length];
-    int offset = limit;
-    int cursor;
-
-    for (int i = 0; i < length; i++) {
-      cursor = i * limit;
-      offset = offset + limit;
-      messages[i] = original.substring(cursor, offset);
+  /**
+   * Sends an array of data packets back across the socket to the client.
+   * @param packets Array of packets to send.
+   * @throws IOException If an error occurred while sending the packet.
+   */
+  private void send(DatagramPacket[] packets) throws IOException {
+    for (DatagramPacket packet: packets) {
+      socket.send(packet);
     }
-
-    return messages;
   }
 
-  private String retrieve(String address) throws IOException {
-    StringBuilder result = new StringBuilder();
-    URL url = new URL(address);
-    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    conn.setRequestMethod("GET");
-    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-    String line;
-    while ((line = reader.readLine()) != null) {
-      result.append(line);
+  /**
+   * Resends the last known payload.
+   * @throws Exception If the last know payload does not exist.
+   */
+  private void resend() throws Exception {
+    if (payload == null) {
+      throw new Exception("payload to retransmit does not exist");
     }
-    return result.toString();
+    this.send(this.payload);
   }
+}
 
-  public static void main(String args[]) {
-    try {
-      Thread thread = new GroupServer();
-      thread.start();
-    } catch (IOException err) {
-      LOGGER.severe(err.getMessage());
-    }
+
+/**
+ * Server that waits for UDP packets, parses them for an external HTTP GET request,
+ * then proxies the contents back to the client via UDP.
+ */
+public class GroupServer {
+  public static void main(String args[]) throws IOException {
+    new GroupServerThread("group_server").start();
   }
 }
